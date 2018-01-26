33879e7e98da56bc8ea2abae0d0cd9358d111709
Diego Matias <dm.diego.bh@gmail.com>
2018-01-26 10:57:13 -0200

    Activity detail page finished

--- src/pages/activity-detail/activity-detail.module.ts
+++ src/pages/activity-detail/activity-detail.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ActivityDetailPage } from './activity-detail';
+import { PipesModule } from '../../pipes/pipes.module';
 
 @NgModule({
   declarations: [
     ActivityDetailPage,
   ],
   imports: [
+    PipesModule,
     IonicPageModule.forChild(ActivityDetailPage),
   ],
 })
--- src/pages/activity-detail/activity-detail.ts
+++ src/pages/activity-detail/activity-detail.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ActivityModel } from '../../model/activity.model';
+import { ActivityProvider } from '../../providers/activity/activity';
 
 /**
  * Generated class for the ActivityDetailPage page.
@@ -8,18 +10,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
+@IonicPage({segment:'activity/:activity_id'})
 @Component({
   selector: 'page-activity-detail',
   templateUrl: 'activity-detail.html',
 })
 export class ActivityDetailPage {
+  activity_id:number;
+  activity:ActivityModel;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private activityProvider:ActivityProvider
+  ) {
+    this.activity_id = this.navParams.get('activity_id');
+    this.activity = this.navParams.get('activity');
+
+    if(this.activity_id && !this.activity){
+      this.getActivity();
+    }
+  }
+
+  getActivity(){
+    this.activityProvider.get(this.activity_id).subscribe((data:ActivityModel)=>{
+      this.activity = data;
+      console.log(data);
+    })
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ActivityDetailPage');
-  }
 
+  }
 }
