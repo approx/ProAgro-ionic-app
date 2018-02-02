495bfc3a6dc1457ce4d1a12145402be11b119650
Diego Matias <dm.diego.bh@gmail.com>
2018-02-02 15:11:09 -0200

    Added total cost to farm info component

--- src/components/farm-info/farm-info.ts
+++ src/components/farm-info/farm-info.ts
@@ -1,4 +1,4 @@
-import { Component,Input } from '@angular/core';
+import { Component,Input,OnInit } from '@angular/core';
 import { FarmModel } from '../../model/farm.model';
 
 /**
@@ -11,15 +11,30 @@ import { FarmModel } from '../../model/farm.model';
   selector: 'farm-info',
   templateUrl: 'farm-info.html'
 })
-export class FarmInfoComponent {
+export class FarmInfoComponent implements OnInit{
 
   @Input() farm:FarmModel;
   text:string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus rhoncus vulputate ligula, sed ullamcorper ex aliquet aliquam.';
-  budget:number = 10000;
+  total_value:number;
 
 
   constructor() {
     console.log('Hello FarmInfoComponent Component');
+  }
+
+  ngOnInit(){
+    this.calculateTotal();
+
+  }
+
+  calculateTotal(){
+    this.total_value = this.farm.capital_tied;
+    if(this.farm.inventory_itens.length>0){
+      for (let i = 0; i < this.farm.inventory_itens.length; i++) {
+          this.total_value+=this.farm.inventory_itens[i].price;
+      }
+    }
+    this.total_value = Math.round(this.total_value*100)/100;
   }
 
 }
