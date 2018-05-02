ccd921838e4ee8739a6663f7d75c8878f57d3c93
Diego Matias <dm.diego.bh@gmail.com>
2018-05-02 10:18:38 -0300

    changed to version 1.3.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,7 +40,7 @@ import { StockUsePage } from '../pages/stock-use/stock-use';
 export class MyApp {
   rootPage:any = ClientListPage.name;
 
-  version = '1.3.1';
+  version = '1.3.2';
 
   loged:boolean = false;
   user:UserModel;
