e0c3361a7b3c8241e9a41fa11cb8329db6b7dfe6
Diego Matias <dm.diego.bh@gmail.com>
2018-05-25 11:27:45 -0300

    changed inventory iten sale, now can select crops when sale a iten

--- src/pages/inventory-iten-sale/inventory-iten-sale.ts
+++ src/pages/inventory-iten-sale/inventory-iten-sale.ts
@@ -8,6 +8,7 @@ import { MessagesProvider } from "../../providers/messages/messages";
 import { BasePage } from "../base/base";
 import { MyApp } from '../../app/app.component';
 import { FarmListPage } from '../../pages/farm-list/farm-list';
+import { CropProvider } from '../../providers/crop/crop';
 
 /**
  * Generated class for the InventoryItenSalePage page.
@@ -37,11 +38,11 @@ import { FarmListPage } from '../../pages/farm-list/farm-list';
      this.sending++;
    }
 
-   public Send(){
+   public Send(propagateByProduction,crops){
      return new Promise((resolve,reject)=>{
        for (let i = 0; i < this.itens.length; i++) {
          console.log(this.itens[i]);
-           this.provider.sell(this.itens[i],this.itens[i].id).subscribe((data)=>{
+           this.provider.sell({...this.itens[i],propagateByProduction:propagateByProduction,crops:crops},this.itens[i].id).subscribe((data)=>{
              this.sended++;
              if(!this.error&&this.sended==this.sending){
                resolve();
@@ -73,8 +74,23 @@ export class InventoryItenSalePage extends BasePage{
   farm:FarmModel;
   farm_id:number;
   maxDate;
+  propagateByProduction=true;
+  useActualCrops=true;
+  actualCrop=[];
+  selectedCrops;
+  crops;
+  filteredCrops;
+  selectedAll;
+  cropSearch;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,private farmProvider:FarmProvider, private inventoryItenProvider:InventoryItenProvider,private message:MessagesProvider) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private farmProvider:FarmProvider,
+    private inventoryItenProvider:InventoryItenProvider,
+    private message:MessagesProvider,
+    private cropProvider:CropProvider
+  ) {
     super(navCtrl);
   }
 
@@ -99,6 +115,7 @@ export class InventoryItenSalePage extends BasePage{
     this.farm = this.navParams.get('farm');
     this.getParams();
     this.setMaxDate();
+    this.getCrops();
   }
 
   Register(){
@@ -109,11 +126,89 @@ export class InventoryItenSalePage extends BasePage{
         }
     }
     this.message.Wait()
-    update.Send().then(()=>{
+    let cropsIds='';
+    this.crops.forEach((crop,i)=>{
+      if(crop.selected){
+        if(cropsIds==''){
+          cropsIds+=crop.id;
+        }else{
+          cropsIds+=';'+crop.id;
+        }
+      }
+    })
+    update.Send(this.propagateByProduction,cropsIds).then(()=>{
       this.message.SuccessAlert('Sucesso vendas cadastradas com suscesso!');
     }).catch(()=>{
       this.message.ErrorAlert();
     })
+  }
+
+  getCrops(){
+    this.cropProvider.getAll().subscribe(
+      (response)=>{
+        this.crops = response.map((crop)=>{
+          return {...crop,selected:false};
+        });
+        // console.log(response)
+        let today = new Date();
+        this.crops.forEach((crop)=>{
+          let cropFinalDate = new Date(crop.final_date);
+          if(cropFinalDate.getTime()>today.getTime()){
+            crop.selected = true;
+          }
+        });
+        this.filteredCrops = this.crops;
+        console.log(this.actualCrop)
+      },(err)=>{}
+    );
+  }
+
+  onSearch(){
+    this.filter();
+  }
+
+  filter(){
+    this.filteredCrops = this.filterCropsByName(this.crops);
+    if(this.selectedAll){
+      this.selectedAll = false;
+      this.selectAll();
+    }
+  }
+
+  selectAll(){
+    if(this.selectedAll){
+      this.filteredCrops.forEach((element,index)=>{
+        element.selected = true;
+      });
+    }else{
+      this.crops.forEach((element,index)=>{
+        element.selected = false;
+      });
+    }
+  }
+
+  useActualCropsChange(){
+    if(this.useActualCrops){
+      this.cropSearch = '';
+      this.filteredCrops = this.crops;
+      this.selectedAll = false;
+      this.crops.forEach((crop)=>{
+        crop.selected = false;
+      });
+      let today = new Date();
+      this.crops.forEach((crop)=>{
+        let cropFinalDate = new Date(crop.final_date);
+        if(cropFinalDate.getTime()>today.getTime()){
+          crop.selected = true;
+        }
+      });
+    }
+  }
+
+  filterCropsByName(crops){
+    return crops.filter((crops)=>{
+      return crops.name.toLowerCase().indexOf(this.cropSearch.toLowerCase())!=-1||crops.field.name.toLowerCase().indexOf(this.cropSearch.toLowerCase())!=-1||this.cropSearch=='';
+    });
   }
 
   getParams(){
