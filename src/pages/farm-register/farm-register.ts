ab2806abf0f89f06d8439d338fcdb6e725727b37
Diego Matias <dm.diego.bh@gmail.com>
2018-05-29 16:32:02 -0300

    fix on save farm

--- src/pages/farm-register/farm-register.ts
+++ src/pages/farm-register/farm-register.ts
@@ -101,7 +101,7 @@ export class FarmRegisterPage extends BasePage{
     if(this.farm.value_ha && this.farm.ha){
       this.farm.capital_tied = this.farm.ha * this.farm.value_ha;
       // this.capital_tied = 'R$ ' + this.farm.capital_tied;
-      this.farm.remuneration = (this.farm.capital_tied*.5)/100;
+      // this.farm.remuneration = (this.farm.capital_tied*.5)/100;
       // this.remuneration = 'R$ ' + this.farm.remuneration;
     }
   }
